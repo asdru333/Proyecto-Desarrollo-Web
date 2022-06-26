f6fdd57f2d6c18742c6484b81d403d33dfe6d030
asdru333 <asdrubal.villegas.molina@hotmail.com>
2022-06-26 11:15:42 -0600

    base para home

--- magma-pizza/src/Pages/Home/index.js
+++ magma-pizza/src/Pages/Home/index.js
@@ -1,5 +1,20 @@
+import portada from "../../Resources/Images/portada.png"
+
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <div className="flex justify-center mt-10">
+        <img className="h-1/2 w-1/2" src={portada} alt="portada"/>
+      </div>
+      <div className="text-center text-pizza text-3xl cel:text-lg my-10">
+        Disfrute de nuestras pizzas innovadoras
+      </div>
+      <div className="text-center text-pizza text-3xl cel:text-lg my-10">
+        Tambien puede disfrutar de las clásicas
+      </div>
+      <div className="text-center text-pizza text-3xl cel:text-lg my-10">
+        Si no puede decidir ¡Dale un vistazo a nuestros combos!
+      </div>
+    </div>
   )
 }
