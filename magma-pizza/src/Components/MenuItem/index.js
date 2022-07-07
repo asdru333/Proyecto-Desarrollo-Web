0706ab0da8bfcc87aa82e1d2909286df9e80e4d1
asdru333 <asdrubal.villegas.molina@hotmail.com>
2022-07-07 11:56:15 -0600

    modificación del tamaño de la pizza

--- magma-pizza/src/Components/MenuItem/index.js
+++ magma-pizza/src/Components/MenuItem/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { GridItemBtn } from '../Buttons';
+import { useState } from "react";
 
 
 function MenuItem({name, description, price, image, alt}){
@@ -12,6 +13,43 @@ function MenuItem({name, description, price, image, alt}){
                 <p>{name}</p>
                 <p className='whitespace-pre-line'>{description}</p>
                 <p>Precio: {price}</p>
+                <GridItemBtn text= 'Agregar al carrito' />
+            </div>
+        </div>
+    </div>
+}
+
+function MenuPizzaItem({name, description, price, image, alt}){
+    const [size, setSize] = useState("Pequeña");
+    const [sizePrice, setSizePrice] = useState(parseInt(price));
+    
+    const handleDropdownChange = (itemValue) => {
+        let itemPrice = 0;
+        if (itemValue === "Mediana")
+            itemPrice = itemPrice + 750
+        else if (itemValue === "Grande")
+            itemPrice = itemPrice + 1500
+        let newPrice = parseInt(price) + itemPrice;
+        setSizePrice(newPrice);
+        setSize(itemValue);
+    }
+
+    return <div>
+        <div className="flex flex-col">
+            <div className="flex justify-center">
+                <img className = "w-56 h-56" src={image} alt={alt} />
+            </div>
+            <div className = "text-center flex flex-col gap-1">
+                <p>{name}</p>
+                <p className='whitespace-pre-line'>{description}</p>
+                <div classname="bg-pizza text-white">
+                    <select onChange={(evt) => handleDropdownChange(evt.target.value)}>
+                        <option classname="bg-pizza text-white" value="Pequeña">Pequeña</option>
+                        <option value="Mediana">Mediana</option>
+                        <option value="Grande">Grande</option>
+                    </select>
+                </div>
+                <p>Precio: {sizePrice}</p>
                 <GridItemBtn text= 'Agregar al carrito' />
             </div>
         </div>
@@ -43,4 +81,4 @@ function HomeItem({description, image, alt})
 }
 
 
-export { MenuItem, CartItem, HomeItem }
+export { MenuItem, CartItem, HomeItem, MenuPizzaItem }
--- magma-pizza/src/Pages/Pizzas/index.js
+++ magma-pizza/src/Pages/Pizzas/index.js
@@ -1,11 +1,11 @@
 import SideMenu from "../../Components/SideMenu";
-import ProductGrid from "../../Components/ProductGrid";
+import {ProductGridPizza} from "../../Components/ProductGrid";
 
 export default function Pizzas() {
     return (
         <div className="py-6 mt-5 flex">
             <SideMenu />
-            <ProductGrid url='http://localhost:7500/pizzas/'/>
+            <ProductGridPizza url='http://localhost:7500/pizzas/'/>
         </div>
     )
 }
